3fe8bd4927d76e5fbd22cbe82edb17cd7d094dae
KekoaM <kai@moritamcvey.com>
2024-06-09 17:53:25 -0700

    ec2 terraform finished

--- main.tf
+++ main.tf
@@ -9,16 +9,129 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
+# Uses env vars for secrets
 provider "aws" {
   region  = "us-west-2"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-08d70e59c07c61a3a"
-  instance_type = "t2.micro"
+# Fetch existing public key from env var 
+variable "PUB_KEY" {
+  type = string
+}
 
+resource "aws_key_pair" "minecraft" {
+  key_name   = "deployer-key"
+  public_key = var.PUB_KEY
+}
+
+
+resource "aws_vpc" "minecraft" {
+  provider = aws
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  assign_generated_ipv6_cidr_block = true
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "minecraft" {
+    vpc_id            = "${aws_vpc.minecraft.id}"
+    availability_zone = "us-west-2a"
+    cidr_block        = "${cidrsubnet(aws_vpc.minecraft.cidr_block, 4, 1)}"
+    map_public_ip_on_launch = true
+    
+    ipv6_cidr_block = "${cidrsubnet(aws_vpc.minecraft.ipv6_cidr_block, 8, 1)}"
+    assign_ipv6_address_on_creation = true
+}
+
+resource "aws_internet_gateway" "minecraft" {
+    provider = aws
+    vpc_id = "${aws_vpc.minecraft.id}"
+}
+
+resource "aws_default_route_table" "minecraft" {
+    provider = aws
+    default_route_table_id = "${aws_vpc.minecraft.default_route_table_id}"
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.minecraft.id}"
+    }
+
+    route {
+        ipv6_cidr_block = "::/0"
+        gateway_id = "${aws_internet_gateway.minecraft.id}"
+    }
+}
+
+resource "aws_route_table_association" "minecraft" {
+    provider = aws
+    subnet_id      = "${aws_subnet.minecraft.id}"
+    route_table_id = "${aws_default_route_table.minecraft.id}"
+}
+
+# Allow ssh and minecraft tcp
+resource "aws_security_group" "minecraft" {
+  provider = aws
+  vpc_id = "${aws_vpc.minecraft.id}"
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    description = "Minecraft"
+    from_port   = 25565
+    to_port     = 25565
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Minecraft"
+    from_port   = 25565
+    to_port     = 25565
+    protocol    = "tcp"
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  egress {
+    description = ""
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = ""
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    ipv6_cidr_blocks = ["::/0"]
+  }
   tags = {
-    Name = var.instance_name
+    Name = "Minecraft"
   }
 }
 
+
+
+resource "aws_instance" "minecraft_server" {
+  ami           = "ami-01619138e5975be33"
+  instance_type = "t4g.medium"
+  key_name = aws_key_pair.minecraft.key_name
+  associate_public_ip_address = true
+  subnet_id = "${aws_subnet.minecraft.id}"
+  ipv6_address_count = 1
+  vpc_security_group_ids = [aws_security_group.minecraft.id]
+
+  tags = {
+    Name = "minecraft"
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,14 @@
 output "instance_id" {
   description = "ID of the EC2 instance"
-  value       = aws_instance.app_server.id
+  value       = aws_instance.minecraft_server.id
 }
 
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.app_server.public_ip
+output "instance_public_ipv4" {
+  description = "Public IPv4 address of the EC2 instance"
+  value       = aws_instance.minecraft_server.public_ip
 }
 
+output "instance_public_ipv6" {
+  description = "Public IPv6 addresses of the EC2 instance"
+  value       = aws_instance.minecraft_server.ipv6_addresses
+}
